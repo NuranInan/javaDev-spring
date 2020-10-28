af931a7a89dd5dfc474fc820a8535fc94e704f2f
NuranInan <nurdingil@gmail.com>
2020-10-28 04:48:51 -0400

    Post construct pre deploy

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/cybertek/services/Selenium.java
@@ -0,0 +1,8 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+public class Selenium implements Course {
+    @Override
+    public void getTeachingHours() { System.out.println("Teaching hours : 20"); }
+}
